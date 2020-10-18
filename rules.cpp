4c863e606f0767a0f79123ed7531d3189938fb54
Vova Mikhaylov <mikhaylov.v.d@yandex.ru>
2020-10-18 14:52:17 +0300

    Add class Rules realisation

---
+++ rules.cpp
@@ -0,0 +1,131 @@
+#include "rules.h"
+
+/* !
+* \Проверка на соответствие предложения с одним из правил
+* \param[in] sentence - предложение и необходимая информация о нем
+* \return - проходит ли предложение по какому-либо из правил (да/нет)
+*/
+bool Rules::checkRules(Sentence &sentence)
+{
+    return (RhetoricalQuestion(sentence) || ExclamotarySentence(sentence) || ChangeNarrative(sentence) || TalkingToYourself(sentence) || Thoughts(sentence) || ChangePersonNarrative(sentence));
+}
+
+/* !
+* \Проверка на наличие в предложении риторического вопроса
+* \param[in] sentence - предложение и необходимая информация о нем
+* \return - проходит ли предложение по этому правилу (true/false)
+*/
+bool Rules::RhetoricalQuestion(Sentence &sentence)
+{
+    return sentence.sentence.contains("?");
+}
+
+/* !
+* \Проверка на наличие в предложении восклицания
+* \param[in] sentence - предложение и необходимая информация о нем
+* \return - проходит ли предложение по этому правилу (true/false)
+*/
+bool Rules::ExclamotarySentence(Sentence &sentence)
+{
+    return sentence.sentence.contains("!");
+}
+
+/* !
+* \Проверка на наличие в предложении смены времени повествования
+* \param[in] sentence - предложение и необходимая информация о нем
+* \return - проходит ли предложение по этому правилу (true/false)
+*/
+bool Rules::ChangeNarrative(Sentence &sentence)
+{
+        if(textTense == "present")
+        {
+            if(sentence.getTense() == sentence.past)
+                return true;
+        }
+        else if(textTense == "past")
+        {
+            if(sentence.getTense() == sentence.present || sentence.getTense() == sentence.future)
+                return true;
+        }
+        else
+            return false;
+}
+
+/* !
+* \Проверка на наличие в предложении конструкции "разговора героя с собой"
+* \param[in] sentence - предложение и необходимая информация о нем
+* \return - проходит ли предложение по этому правилу (true/false)
+*/
+bool Rules::TalkingToYourself(Sentence &sentence)
+{
+        QList <QString> structure = {
+        "myself",
+        "himself",
+        "herself",
+        "yourself",
+        "ourselves",
+        "yourselves",
+        "themselves"
+        };
+
+        for (int i = 0; i < structure.size(); i++)
+        {
+            if(sentence.sentence.contains(structure[i]))
+            {
+                if(!sentence.sentence.contains("self that"))
+                    return true;
+            }
+        }
+        return false;
+}
+
+/* !
+* \Проверка на наличие в предложении мыслей героев
+* \param[in] sentence - предложение и необходимая информация о нем
+* \return - проходит ли предложение по этому правилу (true/false)
+*/
+bool Rules::Thoughts(Sentence &sentence)
+{
+        QList <QString> structure = {
+            "remember",
+            "remembered",
+            "was remembering",
+            "were remembering",
+            "think",
+            "thought",
+            "was thinking",
+            "were thinking"
+        };
+
+        for(int i = 0; i < structure.size(); i++)
+        {
+            if(sentence.sentence.contains(structure[i]))
+            {
+                if(!sentence.sentence.contains("that"))
+                    return true;
+            }
+        }
+
+        return false;
+}
+
+/* !
+* \Проверка на наличие в предложении смены лица повествования
+* \param[in] sentence - предложение и необходимая информация о нем
+* \return - проходит ли предложение по этому правилу (true/false)
+*/
+bool Rules::ChangePersonNarrative(Sentence &sentence)
+{
+        if(textPerson == "first")
+        {
+            if(sentence.getPerson() == sentence.second)
+                return true;
+        }
+        else if (textPerson == "third")
+        {
+            if(sentence.getPerson() == sentence.first || sentence.getPerson() == sentence.second)
+                return true;
+        }
+        else
+            return false;
+}
